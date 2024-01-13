7069266c6b46ad1b2d29c1d77831b7f5d56b0a0a
juniorberkay <7ubisoft@gmail.com>
2024-01-13 12:06:51 +0300

    update   CSS

---
+++ index.js
@@ -0,0 +1,36 @@
+
+let input = document.querySelector(".input")
+let content = document.querySelector(".content")
+let submit = document.querySelector(".btn2");
+// let todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []
+
+let todos;
+
+if (localStorage.getItem("todos")) {
+    todos = JSON.parse(localStorage.getItem("todos"))
+} else {
+    todos = []
+}
+
+
+
+
+submit.addEventListener("click", function () {
+
+    if (input.value === "") {
+        alert("YOU MUST  ADD SOMETHİNK")
+    } else {
+        alert("Congratulations Sent")
+    }
+
+    let todo = {
+        id: Math.floor(Math.random() * 9999),
+        title: input.value,
+        status: "todo",
+        içerik: content.value
+    }
+    content.value = "";
+    input.value = "";
+    todos.push(todo)
+    localStorage.setItem("todos", JSON.stringify(todos))
+})
